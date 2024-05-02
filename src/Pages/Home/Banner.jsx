6f083b7ac5fd3bc9b64712c350b9bc2b2c9babc5
nhwnahid007 <nhwnahid@gmail.com>
2024-05-02 17:58:58 +0600

    added about us page

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import img1 from "../../assets/images/banner/1.jpg";
 import img2 from "../../assets/images/banner/2.jpg";
@@ -20,17 +20,17 @@ const Banner = () => {
   }, [images.length]);
 
   return (
-    <div className="carousel w-full relative">
+    <div className="carousel  w-full relative">
       <div
         id="slide1"
-        className={`carousel-item relative w-full ${
+        className={`carousel-item  relative w-full ${
           currentSlide === 0 ? "" : "hidden"
         }`}
       >
-        <img src={img1} className="w-full" />
+        <img src={img1} className="w-full rounded-xl lg:h-[660px]" />
         <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
           <div className="text-white space-y-7 pl-12 w-1/2">
-            <h2 className="text-6xl font-bold">
+            <h2 className="md:text-6xl font-bold">
               Affordable Price For Car Servicing
             </h2>
             <p>
@@ -74,10 +74,10 @@ const Banner = () => {
           currentSlide === 1 ? "" : "hidden"
         }`}
       >
-        <img src={img2} className="w-full" />
+        <img src={img2} className="w-full rounded-xl lg:h-[660px]" />
         <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                     <div className='text-white space-y-7 pl-12 w-1/2'>
-                        <h2 className='text-6xl font-bold'>Affordable Price For Car Servicing</h2>
+                        <h2 className='md:text-6xl font-bold'>Affordable Price For Car Servicing</h2>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                         <div>
                             <button className="btn btn-primary mr-5">Discover More</button>
@@ -114,10 +114,10 @@ const Banner = () => {
           currentSlide === 2 ? "" : "hidden"
         }`}
       >
-        <img src={img3} className="w-full" />
+        <img src={img3} className="w-full rounded-xl lg:h-[660px]" />
         <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                     <div className='text-white space-y-7 pl-12 w-1/2'>
-                        <h2 className='text-6xl font-bold'>Affordable Price For Car Servicing</h2>
+                        <h2 className='md:text-6xl font-bold'>Affordable Price For Car Servicing</h2>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                         <div>
                             <button className="btn btn-primary mr-5">Discover More</button>
@@ -154,10 +154,10 @@ const Banner = () => {
           currentSlide === 3 ? "" : "hidden"
         }`}
       >
-        <img src={img4} className="w-full" />
+        <img src={img4} className="w-full rounded-xl lg:h-[660px]" />
         <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                     <div className='text-white space-y-7 pl-12 w-1/2'>
-                        <h2 className='text-6xl font-bold'>Affordable Price For Car Servicing</h2>
+                        <h2 className='md:text-6xl font-bold'>Affordable Price For Car Servicing</h2>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                         <div>
                             <button className="btn btn-primary mr-5">Discover More</button>
@@ -194,10 +194,10 @@ const Banner = () => {
           currentSlide === 4 ? "" : "hidden"
         }`}
       >
-        <img src={img5} className="w-full" />
+        <img src={img5} className="w-full rounded-xl lg:h-[660px]" />
         <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                     <div className='text-white space-y-7 pl-12 w-1/2'>
-                        <h2 className='text-6xl font-bold'>Affordable Price For Car Servicing</h2>
+                        <h2 className='md:text-6xl font-bold'>Affordable Price For Car Servicing</h2>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                         <div>
                             <button className="btn btn-primary mr-5">Discover More</button>
@@ -234,10 +234,10 @@ const Banner = () => {
           currentSlide === 5 ? "" : "hidden"
         }`}
       >
-        <img src={img6} className="w-full" />
+        <img src={img6} className="w-full rounded-xl lg:h-[660px]" />
         <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
                     <div className='text-white space-y-7 pl-12 w-1/2'>
-                        <h2 className='text-6xl font-bold'>Affordable Price For Car Servicing</h2>
+                        <h2 className='md:text-6xl font-bold'>Affordable Price For Car Servicing</h2>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
                         <div>
                             <button className="btn btn-primary mr-5">Discover More</button>
