940af752ee62d4c7e737deba11f33af8b68d4818
nhwnahid007 <nhwnahid@gmail.com>
2024-05-05 10:39:30 +0600

    added about section

--- src/Pages/Home/About.jsx
+++ src/Pages/Home/About.jsx
@@ -4,7 +4,7 @@ import parts from '../../assets/images/about_us/parts.jpg'
 const About = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row">
+      <div className="hero-content flex-col lg:flex-row gap-20">
         <div className='lg:w-1/2 relative'>
             <img
               src={person}
@@ -12,11 +12,11 @@ const About = () => {
             />
             <img
               src={parts}
-              className="w-1/2 absolute right-5 top-1/2 rounded-lg border-4 border-white shadow-2xl"
+              className="w-1/2 absolute right-5 top-1/2 rounded-lg border-8 border-white shadow-2xl"
             />
         </div>
-        <div className='lg:w-1/2 space-y-5'>
-            <h1 className="text-3xl text-orange-500 font-bold"></h1>
+        <div className='lg:w-1/2 space-y-5 p-4'>
+            <h1 className="text-3xl text-orange-500 font-bold">About Us</h1>
           <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
           <p className="py-6">
             There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don&apos;t look even slightly believable. 
